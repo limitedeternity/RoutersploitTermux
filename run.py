3b6172980f1e53037ab7fb6dbc49849d037435cb
Mrcose <45144399+Mrcose@users.noreply.github.com>
2020-08-04 07:35:05 +0700

    Update run.py

--- run.py
+++ run.py
@@ -5,16 +5,6 @@ import sys
 from time import sleep
 from core.rsfcore import *
 
-print("Are you sure want to install routersploit? ( y or n )")
-permission = input(">>> ")
-
-if permission == 'y':
-	print("Now installing routersploit...")
-	sleep(2)
-	runscript()
-
-elif permission == 'n':
-	print("Aborting download...")
-
-else:
-	print("Dont know lang... Exiting...")
+print("Installing Routersploit...")
+sleep(1)
+runscript()
